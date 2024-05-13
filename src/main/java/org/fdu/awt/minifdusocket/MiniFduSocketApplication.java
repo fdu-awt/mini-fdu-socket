82d6d8a47da0c2bdcf77f4e7c07d6f482035c96d
zouyi73 <2168603151@qq.com>
2024-05-13 13:39:46 +0800

    init

---
+++ src/main/java/org/fdu/awt/minifdusocket/MiniFduSocketApplication.java
@@ -0,0 +1,13 @@
+package org.fdu.awt.minifdusocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniFduSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniFduSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fdu/awt/minifdusocket/MiniFduSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fdu.awt.minifdusocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniFduSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
