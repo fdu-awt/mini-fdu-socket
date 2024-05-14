afeb05871f38ca0a1dbc74acfa49b097fb2b9f26
zouyi73 <2168603151@qq.com>
2024-05-15 00:37:35 +0800

    add webConfig

---
+++ src/main/java/org/fdu/awt/minifdusocket/config/WebConfig.java
@@ -0,0 +1,18 @@
+package org.fdu.awt.minifdusocket.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootConfiguration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOriginPatterns("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
