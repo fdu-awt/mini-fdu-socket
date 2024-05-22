865f8560c0b4023bec22a252c998d0f018340b1b
zmark <351479188@qq.com>
2024-05-23 02:29:33 +0800

    feat(IHistoryMessageService): 新增videoChatEnd

--- src/main/java/org/fdu/awt/minifdusocket/service/IHistoryMessageService.java
+++ src/main/java/org/fdu/awt/minifdusocket/service/IHistoryMessageService.java
@@ -3,11 +3,17 @@ package org.fdu.awt.minifdusocket.service;
 import org.fdu.awt.minifdusocket.bo.historyMessage.req.MessageSendReq;
 import org.fdu.awt.minifdusocket.bo.historyMessage.resp.MessageShowResp;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface IHistoryMessageService {
 
     void save(MessageSendReq messageSendReq);
+
+    /**
+     * @param durationInSec 通话时长(秒)
+     */
+    void videoChatEnd(Long localId, Long remoteId, Long durationInSec, Timestamp startTime);
 
     //获取历史聊天信息
     List<MessageShowResp> getHistoryMessages(Long localId, Long remoteId);
