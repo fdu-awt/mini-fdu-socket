4451529c3c36ace4f05b070ac395e9a6c8789360
zmark <351479188@qq.com>
2024-05-23 02:23:47 +0800

    refactor: 视频通话时长的内容存在数据库中

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,6 @@ VALUES (2, 1, 'Hi!', '2024-05-01 12:05:00', 'text');
 INSERT INTO `history_message` (`local_id`, `remote_id`, `content`, `time_stamp`, `type`)
 VALUES (1, 2, 'How are you today?', '2024-05-01 12:10:00', 'text');
 INSERT INTO `history_message` (`local_id`, `remote_id`, `content`, `time_stamp`, `type`)
-VALUES (2, 3, '01:01:01', '2024-05-01 12:04:00', 'video');
+VALUES (2, 3, '视频通话时长: 01:01:01', '2024-05-01 12:04:00', 'video');
 INSERT INTO `history_message` (`local_id`, `remote_id`, `content`, `time_stamp`, `type`)
-VALUES (2, 4, '02:05', '2024-05-01 12:08:00', 'video');
+VALUES (2, 4, '视频通话时长: 02:05', '2024-05-01 12:08:00', 'video');
