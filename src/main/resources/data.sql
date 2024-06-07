b5a2dbf32007762e8f8cec32439ad814d9877e04
zmark <351479188@qq.com>
2024-06-07 12:04:01 +0800

    data: 聊天信息

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,7 +11,8 @@ VALUES (1, 2, 'How are you today?', '2024-05-01 12:10:00', 'text');
 INSERT INTO `history_message` (`local_id`, `remote_id`, `content`, `time_stamp`, `type`)
 VALUES (2, 3, '视频通话时长: 01:01:01', '2024-05-01 12:04:00', 'video');
 INSERT INTO `history_message` (`local_id`, `remote_id`, `content`, `time_stamp`, `type`)
-VALUES (2, 4, '视频通话时长: 02:05', '2024-05-01 12:08:00', 'video'),
-       (2, 4, '视频通话：已拒绝', '2024-05-01 12:09:00', 'video'),
+VALUES (2, 4, '视频通话时长: 02:05', '2024-05-04 12:08:00', 'video'),
+       (2, 4, '视频通话：已拒绝', '2024-04-29 12:09:00', 'video'),
+       (2, 1, '视频通话时长: 02:05', '2024-05-01 12:08:00', 'video'),
        (1, 2, '视频通话：已拒绝', '2024-05-01 12:09:00', 'video'),
        (2, 1, '视频通话：已拒绝', '2024-05-01 12:10:00', 'video');
